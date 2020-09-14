8988d488220fd4fab517a8df5de6961b333592b7
Jake <jithornton1996@gmail.com>
2020-09-14 17:52:34 -0400

    Resource definition

--- HaloMCCEditor/Source/Blam/Resources/IndexBufferDefinition.cs
+++ HaloMCCEditor/Source/Blam/Resources/IndexBufferDefinition.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HaloMCCEditor.Source.Blam
+namespace HaloMCCEditor.Core.Blam.Resources
 {
-    public class BlamRenderGeometryApiResourceDefinition
+    public class IndexBufferDefinition
     {
-
     }
 }
---
+++ HaloMCCEditor/Source/Blam/Resources/VertexBufferDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaloMCCEditor.Core.Blam.Resources
+{
+    public class VertexBufferDefinition
+    {
+    }
+}
